5d6d6af0859bd054909daecc893c7cd7ab77a9af
Magdiel Mora <m41k80@icloud.com>
2024-09-25 09:32:38 -0500

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn restaurant.wsgi
+web: python3 manage.py collectstatic && gunicorn restaurant.wsgi
--- laguadalupana/settings.py
+++ laguadalupana/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'restaurant',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,3 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'restaurant/static'),
 ]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
