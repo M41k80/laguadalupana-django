e6ec0bca6e29621a4be80e657fe76f14bb76426b
Magdiel Mora <m41k80@icloud.com>
2024-09-25 09:22:45 -0500

    allowed

--- laguadalupana/settings.py
+++ laguadalupana/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@ho3hfi2nn%p&s)rb$kmfv431x!y24_u5t6*^#it08=ex73uhh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','web-production-a058.up.railway.app']
 
 
 # Application definition
